2d979f7fb8a8935f55954be305acb2a5bb35880e
AlignedCookie88 <52969840+AlignedCookie88@users.noreply.github.com>
2024-08-13 15:57:14 +0100

    Initial commit

---
+++ src/main/java/com/alignedcookie88/clockstopper/ClockStopper.java
@@ -0,0 +1,82 @@
+package com.alignedcookie88.clockstopper;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.text.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static net.minecraft.server.command.CommandManager.*;
+
+public class ClockStopper implements ModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("ClockStopper");
+
+    public static boolean ticking = true;
+
+    public static boolean forceStopTicks = false;
+
+    @Override
+    public void onInitialize() {
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+
+            dispatcher.register(
+                    literal("clockstopper").then(
+                            literal("stop")
+                                    .requires(source -> source.hasPermissionLevel(4))
+                                    .executes(
+                                    context -> {
+                                        if (forceStopTicks) {
+                                            context.getSource().sendError(Text.literal("The clock is already stopped."));
+                                            return 1;
+                                        }
+
+                                        forceStopTicks = true;
+                                        context.getSource().sendMessage(Text.literal("Stopped the clock."));
+                                        LOGGER.info("The clock has been forcefully stopped by {}.", context.getSource().getDisplayName().getString());
+
+                                        return 1;
+                                    }
+                            )
+                    ).then(
+                            literal("start")
+                                    .requires(source -> source.hasPermissionLevel(4))
+                                    .executes(
+                                    context -> {
+                                        if (!forceStopTicks) {
+                                            context.getSource().sendError(Text.literal("The clock is not stopped."));
+                                            return 1;
+                                        }
+
+                                        forceStopTicks = false;
+                                        context.getSource().sendMessage(Text.literal("Started the clock."));
+                                        LOGGER.info("The clock has been restarted by {}.", context.getSource().getDisplayName().getString());
+
+                                        return 1;
+                                    }
+                            )
+                    ).then(
+                            literal("about")
+                                    .executes(
+                                            context -> {
+
+                                                context.getSource().sendMessage(
+                                                        Text.literal("ClockStopper is a fabric mod developed by AlignedCookie88 that stops the game's tick cycle when no players are online. This allows the server to use practically zero system resources when no players are online. The source code is available at https://github.com/AlignedCookie88/ClockStopper.")
+                                                );
+
+                                                return 1;
+                                            }
+                                    )
+                    )
+            );
+
+        });
+
+    }
+
+    public static int playerCount(MinecraftServer server) {
+        return server.getPlayerManager().getPlayerList().size();
+    }
+}
---
+++ src/main/java/com/alignedcookie88/clockstopper/mixin/MinecraftServerMixin.java
@@ -0,0 +1,49 @@
+package com.alignedcookie88.clockstopper.mixin;
+
+import com.alignedcookie88.clockstopper.ClockStopper;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.ServerNetworkIo;
+import net.minecraft.server.dedicated.MinecraftDedicatedServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.BooleanSupplier;
+
+@Mixin(MinecraftServer.class)
+public abstract class MinecraftServerMixin {
+
+    @Shadow public abstract ServerNetworkIo getNetworkIo();
+
+    @Inject(method = "tickWorlds", at = @At("HEAD"), cancellable = true)
+    public void tickWorlds(BooleanSupplier shouldKeepTicking, CallbackInfo ci) {
+
+        int playerCount = ClockStopper.playerCount((MinecraftServer) (Object) this);
+
+        if (ClockStopper.ticking) {
+
+            if (playerCount == 0) {
+                ClockStopper.LOGGER.info("Stopping clock, no players are online.");
+                ClockStopper.ticking = false;
+            }
+
+            if (ClockStopper.forceStopTicks) {
+                ClockStopper.ticking = false;
+            }
+
+        } else {
+
+            if (playerCount > 0 && !ClockStopper.forceStopTicks) {
+                ClockStopper.LOGGER.info("Restarting clock, a player has joined.");
+                ClockStopper.ticking = true;
+            } else {
+                this.getNetworkIo().tick(); // Players cannot join unless this is called.
+                ci.cancel();
+            }
+
+        }
+
+    }
+}
